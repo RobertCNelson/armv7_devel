14dbe64aa82047dab44af256e8ec8882bb4844c5
Robert Nelson <robertcnelson@gmail.com>
2013-03-08 10:19:01 -0600

    scripts: sync with master
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -47,11 +47,6 @@ cleanup () {
 
 arm () {
 	echo "dir: arm"
-	${git} "${DIR}/patches/arm/0001-kbuild-deb-pkg-set-host-machine-after-dpkg-gencontro.patch"
-
-	#Status: v2 Review:
-	#http://lists.infradead.org/pipermail/linux-arm-kernel/2012-August/112440.html
-	${git} "${DIR}/patches/arm/0002-arm-add-definition-of-strstr-to-decompress.c.patch"
 }
 
 arm
