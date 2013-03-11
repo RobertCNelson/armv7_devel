805a893a16427c7d8a529307f1e5f0b94d868669
Robert Nelson <robertcnelson@gmail.com>
2013-03-11 12:39:16 -0500

    config: sync with v3.6.9 tree config options
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -22,7 +22,7 @@ config="at91_dt_defconfig"
 #Kernel/Build
 KERNEL_REL=3.9
 KERNEL_TAG=${KERNEL_REL}-rc2
-BUILD=sama5-armv7-d0.5
+BUILD=sama5-armv7-d0.6
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA="47b3bc907328db968bc9b43c41f48f8d1e140750"
