418e5e3f5c2aa39a3c5aa2d5ef3a1cea42552fa3
Robert Nelson <robertcnelson@gmail.com>
2013-03-14 15:59:30 -0500

    bump: kernel sha
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -22,10 +22,10 @@ config="at91_dt_defconfig"
 #Kernel/Build
 KERNEL_REL=3.9
 KERNEL_TAG=${KERNEL_REL}-rc2
-BUILD=sama5-armv7-d0.6
+BUILD=sama5-armv7-d0.7
 
 #v3.X-rcX + upto SHA
-#KERNEL_SHA="47b3bc907328db968bc9b43c41f48f8d1e140750"
+KERNEL_SHA="40e4591d949469d089714518385e4a99071a1762"
 
 #git branch
 BRANCH="v3.9.x-sama5-armv7"
