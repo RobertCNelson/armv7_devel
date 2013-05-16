e6da06f303f2c04a161efa609005dca219daff34
Robert Nelson <robertcnelson@gmail.com>
2013-05-16 16:16:48 -0500

    sama5: boot v3.10-rc1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -16,15 +16,15 @@ fi
 config="at91_dt_defconfig"
 
 #Kernel/Build
-KERNEL_REL=3.9
-KERNEL_TAG=${KERNEL_REL}.2
-BUILD=sama5-armv7-d0.15
+KERNEL_REL=3.10
+KERNEL_TAG=${KERNEL_REL}-rc1
+BUILD=sama5-armv7-d0.1
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA="de55eb1d60d2ed0f1ba5e13226d91b3bfbe1c108"
 
 #git branch
-BRANCH="v3.9.x-sama5-armv7"
+BRANCH="v3.10.x-sama5-armv7"
 
 BUILDREV=1.0
 DISTRO=cross
