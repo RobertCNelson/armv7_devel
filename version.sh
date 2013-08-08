f8de2ebca3c2807b31dfcc578b4b805a8b85fbb6
Robert Nelson <robertcnelson@gmail.com>
2013-08-08 16:08:26 -0500

    config: enable Android, Alex should have a follow up rootfs right?
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -29,7 +29,7 @@ linaro_toolchain="cortex_gcc_4_8"
 #Kernel/Build
 KERNEL_REL=3.11
 KERNEL_TAG=${KERNEL_REL}-rc4
-BUILD=sama5-armv7-x0.1
+BUILD=sama5-armv7-x0.2
 
 #v3.X-rcX + upto SHA
 KERNEL_SHA=""
