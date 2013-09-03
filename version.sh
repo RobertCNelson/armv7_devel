8bf62e6701198912d5e62e30be408d61e5bb6560
Robert Nelson <robertcnelson@gmail.com>
2013-09-03 11:14:30 -0500

    v3.12.x-sama5-armv7 branch
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -30,13 +30,13 @@ linaro_toolchain="cortex_gcc_4_8"
 #Kernel/Build
 KERNEL_REL=3.11
 KERNEL_TAG=${KERNEL_REL}
-BUILD=sama5-armv7-x0.7
+BUILD=sama5-armv7-x0
 
 #v3.X-rcX + upto SHA
 KERNEL_SHA=""
 
 #git branch
-BRANCH="v3.11.x-sama5-armv7"
+BRANCH="v3.12.x-sama5-armv7"
 
 BUILDREV=1.0
 DISTRO=cross
