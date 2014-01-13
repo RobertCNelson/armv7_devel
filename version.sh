37da174a6b242eaf18c05f3ff4a4a30f48868e97
Robert Nelson <robertcnelson@gmail.com>
2014-01-13 12:03:58 -0600

    kernel bump: v3.12.7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,8 +28,8 @@ linaro_toolchain="cortex_gcc_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.12
-KERNEL_TAG=${KERNEL_REL}.6
-BUILD=sama5-armv7-r13
+KERNEL_TAG=${KERNEL_REL}.7
+BUILD=sama5-armv7-r14
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
