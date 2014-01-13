918cc457a862012478459779913e7b6e64540c3d
Robert Nelson <robertcnelson@gmail.com>
2014-01-13 11:05:20 -0600

    kernel bump: v3.13-rc8
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,8 +28,8 @@ linaro_toolchain="cortex_gcc_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.13
-KERNEL_TAG=${KERNEL_REL}-rc5
-BUILD=sama5-armv7-r4
+KERNEL_TAG=${KERNEL_REL}-rc8
+BUILD=sama5-armv7-r5
 
 #v3.X-rcX + upto SHA
 KERNEL_SHA=""
