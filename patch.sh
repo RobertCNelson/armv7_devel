079ba83db5cb5d810702b96d5810fac45cd7dca7
Robert Nelson <robertcnelson@gmail.com>
2014-01-27 14:52:06 -0600

    sync with: https://github.com/linux4sam/linux-at91/tree/linux-3.10-at91
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -25,10 +25,12 @@
 git="git am"
 
 git_patchset="git://github.com/RobertCNelson/linux.git"
+atmel_patchset="git://github.com/linux4sam/linux-at91.git"
 if [ -f ${DIR}/system.sh ] ; then
 	. ${DIR}/system.sh
 	if [ "${GIT_OVER_HTTP}" ] ; then
 		git_patchset="https://github.com/RobertCNelson/linux.git"
+		atmel_patchset="https://github.com/linux4sam/linux-at91.git"
 	fi
 fi
 
@@ -66,6 +68,7 @@ atmel_fixes () {
 atmel_SAMA5D3 () {
 	echo "pulling linux-3.10-at91"
 	git pull ${GIT_OPTS} ${git_patchset} linux-3.10-at91
+	git pull ${GIT_OPTS} ${atmel_patchset} linux-3.10-at91
 
 	#git checkout v3.10 -b tmp
 	#git pull --no-edit git://github.com/RobertCNelson/linux.git linux-3.10-at91
@@ -77,17 +80,17 @@ atmel_SAMA5D3 () {
 	#git format-patch -10 -o /opt/github/armv7_devel/patches/atmel_SAMA5/
 	#git checkout master -f ; git branch -D tmp
 
-	echo "dir: atmel_SAMA5D3"
-	${git} "${DIR}/patches/atmel_SAMA5/0001-ARM-at91-sama5d3-add-definition-for-usart-base-addre.patch"
-	${git} "${DIR}/patches/atmel_SAMA5/0002-ARM-at91-include-sama5d3.h-into-hardware.h.patch"
-	${git} "${DIR}/patches/atmel_SAMA5/0003-pwm-atmel-pwm-Add-Atmel-PWM-controller-driver.patch"
-	${git} "${DIR}/patches/atmel_SAMA5/0004-of-Add-Atmel-PWM-controller-device-tree-binding.patch"
-	${git} "${DIR}/patches/atmel_SAMA5/0005-ARM-at91-add-PWM-clock.patch"
-	${git} "${DIR}/patches/atmel_SAMA5/0006-ARM-at91-add-PWM-device-node.patch"
-	${git} "${DIR}/patches/atmel_SAMA5/0007-arm-at91-at91sam9m10g45ek-switch-to-PWM-leds.patch"
-	${git} "${DIR}/patches/atmel_SAMA5/0008-ARM-at91-select-AT91_PROGRAMMABLE_CLOCKS-for-buildin.patch"
-	${git} "${DIR}/patches/atmel_SAMA5/0009-crypto-authenc-Find-proper-IV-address-in-ablkcipher-.patch"
-	${git} "${DIR}/patches/atmel_SAMA5/0010-mmc-atmel-mci-fix-timeout-errors-in-SDIO-mode-when-u.patch"
+	#echo "dir: atmel_SAMA5D3"
+	#${git} "${DIR}/patches/atmel_SAMA5/0001-ARM-at91-sama5d3-add-definition-for-usart-base-addre.patch"
+	#${git} "${DIR}/patches/atmel_SAMA5/0002-ARM-at91-include-sama5d3.h-into-hardware.h.patch"
+	#${git} "${DIR}/patches/atmel_SAMA5/0003-pwm-atmel-pwm-Add-Atmel-PWM-controller-driver.patch"
+	#${git} "${DIR}/patches/atmel_SAMA5/0004-of-Add-Atmel-PWM-controller-device-tree-binding.patch"
+	#${git} "${DIR}/patches/atmel_SAMA5/0005-ARM-at91-add-PWM-clock.patch"
+	#${git} "${DIR}/patches/atmel_SAMA5/0006-ARM-at91-add-PWM-device-node.patch"
+	#${git} "${DIR}/patches/atmel_SAMA5/0007-arm-at91-at91sam9m10g45ek-switch-to-PWM-leds.patch"
+	#${git} "${DIR}/patches/atmel_SAMA5/0008-ARM-at91-select-AT91_PROGRAMMABLE_CLOCKS-for-buildin.patch"
+	#${git} "${DIR}/patches/atmel_SAMA5/0009-crypto-authenc-Find-proper-IV-address-in-ablkcipher-.patch"
+	#${git} "${DIR}/patches/atmel_SAMA5/0010-mmc-atmel-mci-fix-timeout-errors-in-SDIO-mode-when-u.patch"
 }
 
 arm
