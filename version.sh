0b258089325ed5faac18d4c730338a65c8dc8104
Robert Nelson <robertcnelson@gmail.com>
2014-02-11 10:22:12 -0600

    kernel bump: v3.13.2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -29,8 +29,8 @@ toolchain="cortex_gcc_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.13
-KERNEL_TAG=${KERNEL_REL}.1
-BUILD=sama5-armv7-r7
+KERNEL_TAG=${KERNEL_REL}.2
+BUILD=sama5-armv7-r8
 
 #v3.X-rcX + upto SHA
 KERNEL_SHA=""
