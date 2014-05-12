6f0e204f5e9153b84c6b8f482db3661246840036
Robert Nelson <robertcnelson@gmail.com>
2014-05-12 17:01:41 -0500

    kernel bump: v3.14.3
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,8 +27,8 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.14
-KERNEL_TAG=${KERNEL_REL}.1
-BUILD=vybrid-armv7-r5
+KERNEL_TAG=${KERNEL_REL}.3
+BUILD=vybrid-armv7-r6
 
 #v3.X-rcX + upto SHA
 KERNEL_SHA=""
