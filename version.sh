a4cf49da6fd007642f0b9b5d83a226eec196d791
Robert Nelson <robertcnelson@gmail.com>
2014-05-13 09:44:17 -0500

    scripts: sync with master of: https://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -17,11 +17,8 @@ if [ "${GIT_NOEDIT}" ] ; then
 	GIT_OPTS="${GIT_OPTS} --no-edit"
 fi
 
-#config="at91_dt_defconfig"
 config="sama5_defconfig"
 
-#toolchain="arm9_gcc_4_7"
-#toolchain="gcc_arm_embedded_4_8"
 #toolchain="gcc_linaro_eabi_4_8"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
