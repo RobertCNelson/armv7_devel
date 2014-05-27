f9aa66be0ea1b5ca567968bc0f5a3374aa594ce1
Robert Nelson <robertcnelson@gmail.com>
2014-05-27 14:42:25 -0500

    kernel bump: v3.15-rc7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.15
-KERNEL_TAG=${KERNEL_REL}-rc5
-BUILD=vybrid-armv7-r3
+KERNEL_TAG=${KERNEL_REL}-rc7
+BUILD=vybrid-armv7-r4
 
 #v3.X-rcX + upto SHA
 KERNEL_SHA=""
