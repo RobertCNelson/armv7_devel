8b1a82271f77b2a7378156cfdbcda911398a56f7
Robert Nelson <robertcnelson@gmail.com>
2015-06-23 11:20:53 -0500

    kernel bump: v4.0.6
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -13,8 +13,8 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
 KERNEL_REL=4.0
-KERNEL_TAG=${KERNEL_REL}
-BUILD=sama5-armv7-r3
+KERNEL_TAG=${KERNEL_REL}.6
+BUILD=sama5-armv7-r4
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
