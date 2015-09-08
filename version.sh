cf10f434d32dfde3c04db28fd4fdacf38e373c0f
Robert Nelson <robertcnelson@gmail.com>
2015-09-08 13:14:53 -0500

    kernel bump: v4.2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 #
 ARCH=$(uname -m)
-CORES=$(getconf _NPROCESSORS_ONLN)
-
-#Debian 7 (Wheezy): git version 1.7.10.4 and later needs "--no-edit"
-unset git_opts
-git_no_edit=$(LC_ALL=C git help pull | grep -m 1 -e "--no-edit" || true)
-if [ ! "x${git_no_edit}" = "x" ] ; then
-	git_opts="--no-edit"
-fi
 
 config="imx_v6_v7_defconfig"
 
@@ -20,16 +12,16 @@ config="imx_v6_v7_defconfig"
 toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
-KERNEL_REL=4.1
-KERNEL_TAG=${KERNEL_REL}.6
-BUILD=vybrid-armv7-r1
+KERNEL_REL=4.2
+KERNEL_TAG=${KERNEL_REL}
+BUILD=vybrid-armv7-r0
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
 #KERNEL_SHA=""
 
 #git branch
-BRANCH="v4.1.x-vybrid-armv7"
+BRANCH="v4.2.x-vybrid-armv7"
 
 DISTRO=cross
 DEBARCH=armhf
