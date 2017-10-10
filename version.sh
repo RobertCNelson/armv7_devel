0d799ad0fabb06c81bd05a6a2dca9dcda66c9b92
Robert Nelson <robertcnelson@gmail.com>
2017-10-10 14:41:08 -0500

    scripts: sync with master of: https://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,11 +28,12 @@ toolchain="gcc_linaro_gnueabihf_6"
 #toolchain="gcc_linaro_aarch64_gnu_6"
 #toolchain="gcc_linaro_aarch64_gnu_7"
 
-#Kernel/Build
+#Kernel
 KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}.36
-BUILD=${build_prefix}3
 kernel_rt=".X-rtY"
+#Kernel Build
+BUILD=${build_prefix}3
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
