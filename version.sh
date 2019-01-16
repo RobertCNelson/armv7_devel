964560c9694b8acb726022310ba6f73e956ca82d
Robert Nelson <robertcnelson@gmail.com>
2019-01-16 15:44:23 -0600

    kernel bump: v4.10.17
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -31,8 +31,8 @@ toolchain="gcc_linaro_gnueabihf_6"
 #toolchain="gcc_arm_aarch64_gnu_8"
 
 #Kernel
-KERNEL_REL=4.9
-KERNEL_TAG=${KERNEL_REL}.135
+KERNEL_REL=4.10
+KERNEL_TAG=${KERNEL_REL}.17
 kernel_rt=".X-rtY"
 #Kernel Build
 BUILD=${build_prefix}1
