a33b503f4ddab72c6b79271a28ef9791c43786b0
Robert Nelson <robertcnelson@gmail.com>
2019-01-16 15:09:48 -0600

    kernel bump: v4.4.170
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -13,20 +13,29 @@ KERNEL_ARCH=arm
 #toolchain="gcc_linaro_eabi_4_8"
 #toolchain="gcc_linaro_eabi_4_9"
 #toolchain="gcc_linaro_eabi_5"
+#toolchain="gcc_linaro_eabi_6"
+#toolchain="gcc_linaro_eabi_7"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
 #toolchain="gcc_linaro_gnueabihf_4_8"
 #toolchain="gcc_linaro_gnueabihf_4_9"
 toolchain="gcc_linaro_gnueabihf_5"
+#toolchain="gcc_linaro_gnueabihf_6"
+#toolchain="gcc_linaro_gnueabihf_7"
+#toolchain="gcc_arm_gnueabihf_8"
 #arm64
 #KERNEL_ARCH=arm64
 #toolchain="gcc_linaro_aarch64_gnu_5"
+#toolchain="gcc_linaro_aarch64_gnu_6"
+#toolchain="gcc_linaro_aarch64_gnu_7"
+#toolchain="gcc_arm_aarch64_gnu_8"
 
-#Kernel/Build
+#Kernel
 KERNEL_REL=4.4
-KERNEL_TAG=${KERNEL_REL}.13
-BUILD=${build_prefix}3
+KERNEL_TAG=${KERNEL_REL}.170
 kernel_rt=".X-rtY"
+#Kernel Build
+BUILD=${build_prefix}3
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -35,6 +44,6 @@ kernel_rt=".X-rtY"
 #git branch
 BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 
-DISTRO=cross
+DISTRO=xross
 DEBARCH=armhf
 #
