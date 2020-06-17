65045a01e1f2e059d7627041b8de087b9f667bb2
Robert Nelson <robertcnelson@gmail.com>
2020-06-17 10:50:31 -0500

    kernel bump: v4.14.184
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -35,10 +35,10 @@ toolchain="gcc_linaro_gnueabihf_6"
 
 #Kernel
 KERNEL_REL=4.14
-KERNEL_TAG=${KERNEL_REL}.144
+KERNEL_TAG=${KERNEL_REL}.184
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}6
+BUILD=${build_prefix}7
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
