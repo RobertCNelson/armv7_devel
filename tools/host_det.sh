044fe2152d98574f89f6dfd71ca958d774b12c7e
Robert Nelson <robertcnelson@gmail.com>
2020-06-17 09:33:51 -0500

    scripts: resync all build scripts with master repo: https://github.com/RobertCNelson/linux-dev
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/host_det.sh
+++ tools/host_det.sh
@@ -414,6 +414,11 @@ debian_regs () {
 			#http://packages.linuxmint.com/index.php
 			deb_distro="bionic"
 			;;
+		ulyana)
+			#20
+			#http://packages.linuxmint.com/index.php
+			deb_distro="focal"
+			;;
 		esac
 
 		#Future Debian Code names:
@@ -442,22 +447,19 @@ debian_regs () {
 			warn_eol_distro=1
 			stop_pkg_search=1
 			;;
-		bionic|eoan|focal)
+		bionic|eoan|focal|groovy)
 			#18.04 bionic: (EOL: April 2023) lts: bionic -> focal
-			#19.10 eoan: (EOL: July, 2020)
-			#20.04 focal: (EOL: 2030) lts: focal -> xyz
+			#19.10 eoan: (EOL: July 2020)
+			#20.04 focal: (EOL: April 2025) lts: focal -> xyz
+			#20.10 groovy: (EOL: July 2021)
 			unset warn_eol_distro
 			;;
-		cosmic|disco)
-			#18.10 cosmic: (EOL: July 18, 2019)
-			#19.04 disco: (EOL: January 23, 2020)
-			warn_eol_distro=1
-			stop_pkg_search=1
-			;;
-		yakkety|zesty|artful)
+		yakkety|zesty|artful|cosmic|disco)
 			#16.10 yakkety: (EOL: July 20, 2017)
 			#17.04 zesty: (EOL: January 2018)
 			#17.10 artful: (EOL: July 2018)
+			#18.10 cosmic: (EOL: July 18, 2019)
+			#19.04 disco: (EOL: January 23, 2020)
 			warn_eol_distro=1
 			stop_pkg_search=1
 			;;
@@ -547,7 +549,7 @@ debian_regs () {
 	if [ "${error_unknown_deb_distro}" ] ; then
 		echo "Unrecognized deb based system:"
 		echo "-----------------------------"
-		echo "Please cut, paste and email to: bugs@rcn-ee.com"
+		echo "Please cut, paste and email to: robertcnelson+bugs@gmail.com"
 		echo "-----------------------------"
 		echo "git: [$(${git_bin} rev-parse HEAD)]"
 		echo "git: [$(cat .git/config | grep url | sed 's/\t//g' | sed 's/ //g')]"
@@ -604,6 +606,9 @@ if [ "x${ARCH}" = "xx86_64" ] ; then
 		ignore_32bit="true"
 		;;
 	gcc_arm_eabi_8|gcc_arm_gnueabihf_8|gcc_arm_aarch64_gnu_8)
+		ignore_32bit="true"
+		;;
+	gcc_arm_eabi_9|gcc_arm_gnueabihf_9|gcc_arm_aarch64_gnu_9)
 		ignore_32bit="true"
 		;;
 	*)
