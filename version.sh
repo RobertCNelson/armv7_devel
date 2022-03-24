611694bccf349a2837116073749ae0775cc5c96e
Robert Nelson <robertcnelson@gmail.com>
2022-03-24 14:16:47 -0500

    kernel bump: v5.12.19
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -8,7 +8,7 @@ build_prefix="-sama5-armv7-r"
 branch_prefix="v"
 branch_postfix=".x-sama5-armv7"
 
-#https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/tree/Documentation/process/changes.rst?h=v5.10-rc1
+#https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/tree/Documentation/process/changes.rst?h=v5.12-rc1
 #arm
 KERNEL_ARCH=arm
 DEBARCH=armhf
@@ -37,8 +37,8 @@ toolchain="gcc_9_arm"
 #toolchain="gcc_11_riscv64"
 
 #Kernel
-KERNEL_REL=5.11
-KERNEL_TAG=${KERNEL_REL}.22
+KERNEL_REL=5.12
+KERNEL_TAG=${KERNEL_REL}.19
 kernel_rt=".X-rtY"
 #Kernel Build
 BUILD=${build_prefix}1
